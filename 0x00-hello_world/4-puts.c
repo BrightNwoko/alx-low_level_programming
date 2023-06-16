a2824335fb68ab67d2cbcf4aa53b12bafe00e6dd
BrightNwoko <brightijnwoko@gmail.com>
2023-06-15 18:15:01 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - print a line of code using puts
+ *
+ * Return: always 0
+*/
+int main(void)
+{
+	puts("\"Prrogramming is like building a multilingual puzzle");
+	retrurn (0);
+}

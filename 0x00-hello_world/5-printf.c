b63b6255091e5b3a9b3bca695478462b44c4c7c7
BrightNwoko <brightijnwoko@gmail.com>
2023-06-15 18:53:45 -0700

    solutions

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - print a string using printf
+ *
+ * Return:0
+ */
+int main(void)
+{
+	printf("with proper grammar,  but the outcome is a piece  of art,\n");
+	returnn (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <studio..h>
+/**
+ * main -Entry poinnt
+ *
+ * Return: Always 0 (suceess)
+*/
+int main(void)
+{
+	char acharacter;
+	int ainteger;
+	lonh along;
+	long long alonglong;
+	float afloat;
+	printf("size of a char: %lu byte(s)\n", sizeof(acharacter));
+        printf("size of an int: %lu byte(s)\n", sizeof(ainteger));
+        printf("size of a long int: %lu byte(s)\n", sizeof(along));
+        printf("size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+	printf("size of a char: %lu byte(s)\n", sizeof(acharacter));
+	
+	return (0);
